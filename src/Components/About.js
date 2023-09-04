138fce134ec9d18077d29acf57c821472b7bee58
Yashwant937363 <137669184+Yashwant937363@users.noreply.github.com>
2023-09-04 16:23:01 +0530

    updated code of app

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,86 +1,174 @@
 import React from 'react';
 
-const About = () => {
-  const containerStyle = {
-    maxWidth: '800px',
-    margin: '0 auto',
-    padding: '20px',
-    fontFamily: 'Arial, sans-serif',
-    borderRadius: '8px',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-  };
-
-  const sectionStyle = {
-    marginBottom: '20px',
-  };
-
-  const headingStyle = {
-    fontSize: '24px',
-    marginBottom: '10px',
-    color: 'wheat',
-  };
-
-  const paragraphStyle = {
-    fontSize: '16px',
-    lineHeight: '1.6',
-    color: 'white',
-  };
-
-  const listStyle = {
-    fontSize: '16px',
-    lineHeight: '1.6',
-    paddingLeft: '20px',
-    color: 'white',
+function AboutSection() {
+  // Define internal CSS styles as an object
+  const styles = {
+    aboutSection: {
+      padding: '20px',
+      borderRadius: '8px',
+      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+      textAlign: 'left',
+      maxWidth: '600px',
+      margin: '20px auto', 
+    },
+    term: {
+      marginTop: '20px',
+    },
+    heading: {
+      fontSize: '18px',
+      marginBottom: '5px',
+      color: '#333',
+    },
+    paragraph: {
+      fontSize: '16px',
+      color: '#666',
+    },
   };
 
   return (
-    <div style={containerStyle}>
-      <div style={sectionStyle}>
-        <h2 style={headingStyle}>About the To-Do List App Project</h2>
-        <p style={paragraphStyle}>
-          The To-Do List App is a beginner-friendly project designed to introduce you to fundamental concepts of web development using React. In this project, you will create a simple yet functional to-do list application that empowers users to manage their tasks efficiently. Through this hands-on experience, you will gain insights into React components, state management, and user interactions.
+    <>
+      <div style={styles.aboutSection}>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>State Management</h3>
+          <p style={styles.paragraph}>
+            The component demonstrates the use of the useState hook to manage various pieces of state within a functional component. State is used to store and update data that can change over time, such as weather information, city name, and styling variables like rotationDegree and direction.
+          </p>
+        </div>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Asynchronous Data Fetching</h3>
+          <p style={styles.paragraph}>
+            The component showcases how to fetch data asynchronously from external APIs using the fetch function and the async/await pattern. It demonstrates error handling for API requests, ensuring a robust user experience.
+          </p>
+        </div>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Lifecycle Management with useEffect</h3>
+          <p style={styles.paragraph}>
+            useEffect is used to manage the component's lifecycle. The component triggers data fetching and manipulation functions when specific dependencies (e.g., city, weatherData, curDate, Hours, Minutes, sunrise, sunset) change. This ensures that data is updated when needed and avoids unnecessary re-rendering.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Dynamic Styling with Inline CSS</h3>
+          <p style={styles.paragraph}>
+            Inline CSS within the style tag is employed for dynamic styling based on the component's state. For instance, it rotates a day/night indicator (rotationDegree) and changes the flex direction (direction) of a section based on the time of day.
+          </p>
+        </div>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Conditional Rendering</h3>
+          <p style={styles.paragraph}>
+            The component conditionally renders content based on the availability of data. If weatherData is null (indicating that data is being fetched), it displays a loading message and spinner. This approach ensures a smooth user experience.
+          </p>
+        </div>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>User Interaction Handling</h3>
+          <p style={styles.paragraph}>
+            The component includes a search bar that allows users to input a city name. User input triggers an event handler (updateWeather) that updates the city state and initiates a data fetch.
+          </p>
+        </div>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Data Formatting and Display</h3>
+          <p style={styles.paragraph}>
+            It demonstrates how to format and display data fetched from external APIs, including temperature, sunrise/sunset times, wind direction, and more. Data is presented clearly and meaningfully to the user.
+          </p>
+        </div>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Error Handling</h3>
+          <p style={styles.paragraph}>
+            The component includes error handling for API requests. It logs error messages to the console and maintains a user-friendly interface even when data fetching fails.
+          </p>
+        </div>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Mathematical Calculations</h3>
+          <p style={styles.paragraph}>
+            The component calculates the degree of rotation for the day/night indicator based on sunrise and sunset times. This involves mathematical calculations to determine the position of the sun/moon icon.
+          </p>
+        </div>
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Reactivity</h3>
+          <p style={styles.paragraph}>
+            The component reacts to changes in the fetched data and updates its display accordingly. For example, it adjusts the layout of elements based on whether it's daytime or nighttime.
+          </p>
+        </div>
+      </div>
+      <div style={styles.aboutSection}>
+        <h2>About Weather Data</h2>
+        <p>
+          Weather data provides important information to help you understand the current weather conditions. Here's what each term means in simple language:
         </p>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Cloud Percentage</h3>
+          <p style={styles.paragraph}>
+            This tells you how much of the sky is covered by clouds. When it's at its maximum, the sky is fully cloudy, and you might not see the sun or the blue sky.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Temperature</h3>
+          <p style={styles.paragraph}>
+            Temperature measures how hot or cold it is outside. It can vary from very cold to very hot, affecting how comfortable you might feel.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Feels Like</h3>
+          <p style={styles.paragraph}>
+            This is what it actually feels like outside, considering factors like humidity and wind. It might be the same as the temperature, or it can feel warmer or cooler.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Humidity</h3>
+          <p style={styles.paragraph}>
+            Humidity measures how much moisture is in the air. High humidity can make it feel muggy, while low humidity makes the air feel dry.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Minimum Temperature</h3>
+          <p style={styles.paragraph}>
+            This is the lowest temperature expected during the day or night. It can help you plan for cooler times.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Maximum Temperature</h3>
+          <p style={styles.paragraph}>
+            This is the highest temperature expected. It can indicate whether it will be a warm or cool day.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Wind Speed</h3>
+          <p style={styles.paragraph}>
+            Wind speed measures how fast the wind is blowing. It can range from gentle breezes to strong gusts.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Wind Direction</h3>
+          <p style={styles.paragraph}>
+            This tells you where the wind is coming from. It helps you know which direction the wind is blowing.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Sunrise</h3>
+          <p style={styles.paragraph}>
+            Sunrise is the moment when the sun appears above the horizon in the morning, marking the start of the day.
+          </p>
+        </div>
+
+        <div style={styles.term}>
+          <h3 style={styles.heading}>Sunset</h3>
+          <p style={styles.paragraph}>
+            Sunset is the time when the sun goes below the horizon in the evening, marking the end of the day.
+          </p>
+        </div>
       </div>
-      <div style={sectionStyle}>
-        <h3 style={headingStyle}>Project Overview:</h3>
-        <p style={paragraphStyle}>
-          In this project, you will build a single-page web application that enables users to:
-        </p>
-        <ul style={listStyle}>
-          <li>Add Tasks: Users can input new tasks they need to accomplish. When they press the "Add" button or hit Enter, the task is added to the list.</li>
-          <li>Mark Tasks as Completed: Each task on the list will have a checkbox next to it. Users can mark tasks as completed by checking the box. A visual indicator will distinguish completed tasks from those that are not completed.</li>
-          <li>Delete Tasks: Users can remove tasks from the list once they are completed or no longer needed. Deleting a task will remove it from the list entirely.</li>
-        </ul>
-      </div>
-      <div style={sectionStyle}>
-        <h3 style={headingStyle}>Key Learning Objectives:</h3>
-        <p style={paragraphStyle}>
-          Through the development of this To-Do List App, you will learn:
-        </p>
-        <ul style={listStyle}>
-          <li>React Components: Gain a solid understanding of React components and their role in building modular user interfaces. You will create components for the overall application, the task list, individual tasks, and the input form.</li>
-          <li>State Management: Explore the concept of state within a React application. You will manage the state of tasks, their completion status, and handle updates effectively.</li>
-          <li>User Interaction: Learn how to capture user interactions, such as button clicks and checkbox changes, and respond to them appropriately by updating the component state.</li>
-          <li>Conditional Rendering: Implement conditional rendering to display different UI elements based on the state of tasks, such as showing completed tasks differently.</li>
-        </ul>
-      </div>
-      <div style={sectionStyle}>
-        <h3 style={headingStyle}>Why This Project Matters:</h3>
-        <ul style={listStyle}>
-          <li>Hands-On Experience: This project provides a practical introduction to core React concepts. By building a real-world application, you will deepen your understanding of these concepts.</li>
-          <li>Foundational Skills: The skills acquired in this project will serve as a solid foundation for more complex React applications and help you grasp more advanced topics.</li>
-          <li>Portfolio Building: Upon completion, you will have a tangible project to showcase in your portfolio, demonstrating your ability to create functional and interactive web applications.</li>
-          <li>Confidence Boost: Successfully completing this project will boost your confidence as a developer, empowering you to take on more challenging projects in the future.</li>
-        </ul>
-      </div>
-      <div style={sectionStyle}>
-        <h3 style={headingStyle}>Prerequisites:</h3>
-        <p style={paragraphStyle}>
-          This project assumes basic knowledge of HTML, CSS, and JavaScript. Familiarity with the React library is helpful but not required, as this project will guide you through the necessary concepts step by step.
-        </p>
-        <p style={paragraphStyle}>Get ready to embark on an exciting journey into the world of React development as you create a To-Do List App that showcases your skills and sets you on the path to becoming a proficient web developer.</p>
-      </div>
-    </div>
+    </>
+
   );
-};
-export default About;
+}
+
+export default AboutSection;
--- src/Components/body.js
+++ src/Components/body.js
@@ -8,76 +8,132 @@ function Body() {
   const [sunrise, setSunrise] = useState(null);
   const [sunset, setSunset] = useState(null);
   const [direction, setDirection] = useState('row');
-  const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'a5518acfe5msh3da494b66705d3ap134865jsnc5e7a74b1f8d',
-      'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-    }
-  };
+  const [curDate, setcurDate] = useState(null);
+  const [Hours, setHours] = useState(null);
+  const [Minutes, setMinutes] = useState(null);
+
   // Fetch weather data
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        setWeatherData(data);
-      } catch (error) {
-        console.error(error);
+  const fetchWeatherData = async () => {
+    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'a5518acfe5msh3da494b66705d3ap134865jsnc5e7a74b1f8d',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
       }
     };
-
-    fetchWeatherData();
-  }, []);
-
-  useEffect(() => {
-    if (weatherData) {
-      const fetchedSunrise = new Date(weatherData.sunrise * 1000);
-      const fetchedSunset = new Date(weatherData.sunset * 1000);
-      setSunrise(fetchedSunrise);
-      setSunset(fetchedSunset);
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error(error);
     }
-  }, [weatherData]);
+  };
+  const fetchTimeData = async () => {
+    const apiKey = 'dp8J89ihXlGHYkJnS8W3oQ==J1Yv0GWZ0F7DxqlI';
+    const apiUrl = `https://api.api-ninjas.com/v1/worldtime?city=${city}`;
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': apiKey,
+          'Content-Type': 'application/json'
+        }
+      });
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        console.error('API Error Response:', errorResponse);
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const result = await response.json();
+      setcurDate(result);
+      setHours(result.hour);
+      setMinutes(result.minute);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  const setupSuntime = () => {
+    const fetchedSunrise = new Date(weatherData.sunrise * 1000);
+    const fetchedSunset = new Date(weatherData.sunset * 1000);
+    let timeZone = curDate.timezone;
+    const timeOptions = { timeZone, hour12: false, hour: 'numeric', minute: 'numeric' };
+    const formattedSunrise = fetchedSunrise.toLocaleTimeString(undefined, timeOptions);
+    const formattedSunset = fetchedSunset.toLocaleTimeString(undefined, timeOptions);
+    const formattedSunriseparts = formattedSunrise.split(":");
+    const formattedSunsetparts = formattedSunset.split(":");
+    setSunrise({
+      hours: formattedSunriseparts[0],
+      minutes: formattedSunriseparts[1],
+    });
+    setSunset({
+      hours: formattedSunsetparts[0],
+      minutes: formattedSunsetparts[1],
+    });
+  }
 
   useEffect(() => {
-    if (sunrise && sunset) {
+    fetchWeatherData();
+    fetchTimeData();
+  }, [city]);
+
+  useEffect(() => {
+    if (weatherData && curDate) {
+      setupSuntime();
+    }
+  }, [weatherData, curDate]);
+
+  useEffect(() => {
+    if (weatherData && curDate && Hours !== null && Minutes !== null && sunrise && sunset) {
       calculateDegree();
+      if (Hours > sunset.hours && Minutes > sunset.minutes) {
+        setDirection('row-reverse');
+      };
     }
   }, [sunrise, sunset]);
 
-  const calculateDegree = () => {
-    let curDate = new Date();
-    let startmin = sunrise.getHours() * 60 + sunrise.getMinutes();
-    let endmin = sunset.getHours() * 60 + sunset.getMinutes();
-    let midmin = curDate.getHours() * 60 + curDate.getMinutes();
-    let diffmin = endmin - startmin;
-    let degree = (midmin-startmin) * 180 / diffmin;
-    if (curDate.getHours() > sunset.getHours() || (curDate.getHours() === sunset.getHours() && curDate.getMinutes() > sunset.getMinutes())) {
-      setDirection('row-reverse');
-    }    
-    console.log(direction)
-    console.log(startmin);
-    console.log(endmin);
-    console.log(midmin);
-    console.log(diffmin);
-    console.log(degree);
 
+  const calculateDegree = () => {
+    let startmin = parseInt(sunrise.hours) * 60 + parseInt(sunrise.minutes);
+    let endmin = parseInt(sunset.hours) * 60 + parseInt(sunset.minutes);
+    let midmin = parseInt(Hours) * 60 + parseInt(Minutes);
+    let diffmin = endmin - startmin;
+    let degree = (midmin - startmin) * 180 / diffmin;
     setRotationDegree(degree);
   }
 
   if (!weatherData) {
-    return <div>Loading...</div>;
+    return (
+      <div className="loading-container">
+        <div className="loading-spinner"></div>
+        <p>Loading...</p>
+      </div>
+    );
   }
 
-  const temperature = weatherData.temp;
-  const feelsLike = weatherData.feels_like;
-  const humidity = weatherData.humidity;
-  const minTemperature = weatherData.min_temp;
-  const maxTemperature = weatherData.max_temp;
-  const cloudPercentage = weatherData.cloud_pct;
-  const windSpeed = weatherData.wind_speed;
-  const windDegrees = weatherData.wind_degrees;
+
+  const updateWeather = (event) => {
+    event.preventDefault();
+    let temp = event.target.elements.city.value;
+    const newCity = temp.charAt(0).toUpperCase() + temp.slice(1);
+    setCity(newCity);
+    setWeatherData(null);
+  }
+  const getWindDirection = (degree) => {
+    const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+    const index = Math.round((degree % 360) / 45);
+    return directions[index];
+  }
+
+  let temperature = weatherData.temp;
+  let feelsLike = weatherData.feels_like;
+  let humidity = weatherData.humidity;
+  let minTemperature = weatherData.min_temp;
+  let maxTemperature = weatherData.max_temp;
+  let cloudPercentage = weatherData.cloud_pct;
+  let windSpeed = weatherData.wind_speed;
+  let windDegrees = getWindDirection(weatherData.wind_degrees);
 
   return (
     <main className="section">
@@ -91,6 +147,15 @@ function Body() {
           }
         `}
       </style>
+      <form className='searchbar' onSubmit={updateWeather}>
+        <input type='search' placeholder='Enter City Name' className='searchfield' name='city' />
+        <button type='submit' className='searchbtn'><i className="bi bi-search"></i></button>
+
+      </form>
+      <h1 className='city'>
+        <span>{city}</span>
+        <span>{Minutes && Hours.toString().padStart(2, '0')}:{Hours && Minutes.toString().padStart(2, '0')}</span>
+      </h1>
       <section id="temp" className="card">
         <span className="feli">Feels Like {feelsLike}°C</span>
         <span className="temperature">{temperature}°C</span>
@@ -114,7 +179,7 @@ function Body() {
         </section>
         <section className="card">
           <span>Wind Direction</span>
-          <span>N</span>
+          <span>{windDegrees}</span>
         </section>
         <section className="card">
           <span>Humidity</span>
@@ -129,8 +194,8 @@ function Body() {
           </div>
         </div>
         <div className='time'>
-          <span>Sunrise<br></br>{sunrise && sunrise.getHours().toString().padStart(2, '0')}:{sunrise && sunrise.getMinutes().toString().padStart(2, '0')}</span>
-          <span>Sunset<br></br>{sunset && sunset.getHours().toString().padStart(2, '0')}:{sunset && sunset.getMinutes().toString().padStart(2, '0')}</span>
+          <span>Sunrise<br></br>{sunrise && sunrise.hours}:{sunrise && sunrise.minutes}</span>
+          <span>Sunset<br></br>{sunset && sunset.hours}:{sunset && sunset.minutes}</span>
         </div>
       </section>
     </main>
